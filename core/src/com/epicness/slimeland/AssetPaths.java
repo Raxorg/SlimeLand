e52afe90c4ca6fc8b64182dc8c5325b229089786
Luis Frontanilla <lrdolucho@gmail.com>
2021-12-13 18:38:24 -0400

    Asset paths addition

--- core/src/com/epicness/slimeland/AssetPaths.java
+++ core/src/com/epicness/slimeland/AssetPaths.java
@@ -2,5 +2,11 @@ package com.epicness.slimeland;
 
 public class AssetPaths {
 
-
+    // Fonts
+    public static final String BIGGER_PIXEL_FONT_PATH = "slimeland/fonts/pixelFont.fnt";
+    // Sprites
+    public static final String SLIME_LEFT_HALF_PATH = "slimeland/images/slimeLeftHalf.png";
+    public static final String SLIME_RIGHT_HALF_PATH = "slimeland/images/slimeRightHalf.png";
+    public static final String SLIME_PATH = "slimeland/images/slime.png";
+    public static final String CHECK_PATH = "slimeland/images/check.png";
 }
