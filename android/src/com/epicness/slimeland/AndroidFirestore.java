7148e97a7a14f0741a43019586644b2ae3d4faf6
Luis Frontanilla <lrdolucho@gmail.com>
2021-12-13 19:32:25 -0400

    Progress with firestore

--- android/src/com/epicness/slimeland/AndroidFirestore.java
+++ android/src/com/epicness/slimeland/AndroidFirestore.java
@@ -1,44 +1,53 @@
 package com.epicness.slimeland;
 
-import androidx.annotation.NonNull;
-
 import com.epicness.firebase.CoreFirestore;
 import com.epicness.firebase.ResultListener;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Transaction;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class AndroidFirestore implements CoreFirestore {
 
+    private final FirebaseFirestore database;
     private final CollectionReference players;
+    private final CollectionReference gameInfo;
 
     public AndroidFirestore() {
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database = FirebaseFirestore.getInstance();
         players = database.collection("players");
+        gameInfo = database.collection("gameInfo");
     }
 
     @Override
     public void registerPlayer(String playerID, ResultListener<Boolean> listener) {
         Map<String, Object> user = new HashMap<>();
         user.put("PlayerID", playerID);
-        players.add(user).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentReference> task) {
-                if (task.isSuccessful()) {
+        addDocument(players, playerID, user, "ERROR REGISTERING PLAYER", listener);
+    }
 
-                } else {
-                    Exception exception = task.getException();
-                    if (exception == null) {
-                        return;
-                    }
-                    System.out.println(task.getException().getMessage());
+    @Override
+    public void claimColor(String color, ResultListener<Boolean> listener) {
+        database.runTransaction((Transaction.Function<Void>) transaction -> {
+            DocumentSnapshot snapshot = transaction.get(gameInfo.document("colors"));
+            return null;
+        });
+    }
+
+    public void addDocument(CollectionReference collectionReference, String documentPath, Map<String, Object> documentInfo,
+                            String errorMessage, ResultListener<Boolean> listener) {
+        collectionReference.document(documentPath).set(documentInfo).addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Exception exception = task.getException();
+                if (exception == null) {
+                    return;
                 }
+                System.out.println(errorMessage + ": " + task.getException().getMessage());
             }
+            listener.onResult(task.isSuccessful());
         });
     }
 }
--- core/src/com/epicness/firebase/CoreFirestore.java
+++ core/src/com/epicness/firebase/CoreFirestore.java
@@ -3,4 +3,6 @@ package com.epicness.firebase;
 public interface CoreFirestore {
 
     void registerPlayer(String playerID, ResultListener<Boolean> listener);
+
+    void claimColor(String color, ResultListener<Boolean> listener);
 }
