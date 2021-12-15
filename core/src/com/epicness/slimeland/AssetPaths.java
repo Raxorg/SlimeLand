decad74fe925db10c7d6385dd091c1fd84a931d2
Luis Frontanilla <lrdolucho@gmail.com>
2021-12-14 20:07:39 -0400

    Factory and tower assets

--- core/src/com/epicness/slimeland/AssetPaths.java
+++ core/src/com/epicness/slimeland/AssetPaths.java
@@ -12,6 +12,12 @@ public class AssetPaths {
     public static final String SLIME_PATH = "slimeland/images/slime.png";
     public static final String CLOUD_PATH = "slimeland/images/cloud.png";
     public static final String CLOUD_SHADOW_PATH = "slimeland/images/cloudShadow.png";
+
+    public static final String FACTORY_LEFT_PATH = "slimeland/images/machines/factoryLeft.png";
+    public static final String FACTORY_RIGHT_PATH = "slimeland/images/machines/factoryRight.png";
+    public static final String TOWER_LEFT_PATH = "slimeland/images/machines/towerLeft.png";
+    public static final String TOWER_RIGHT_PATH = "slimeland/images/machines/towerRight.png";
+
     public static final String BLUE_ORNAMENT_PATH = "slimeland/images/blueOrnament.png";
     public static final String GREEN_ORNAMENT_PATH = "slimeland/images/greenOrnament.png";
     public static final String ORANGE_ORNAMENT_PATH = "slimeland/images/orangeOrnament.png";
--- core/src/com/epicness/slimeland/game/GameAssets.java
+++ core/src/com/epicness/slimeland/game/GameAssets.java
@@ -3,12 +3,16 @@ package com.epicness.slimeland.game;
 import static com.epicness.slimeland.AssetPaths.BLUE_ORNAMENT_PATH;
 import static com.epicness.slimeland.AssetPaths.CLOUD_PATH;
 import static com.epicness.slimeland.AssetPaths.CLOUD_SHADOW_PATH;
+import static com.epicness.slimeland.AssetPaths.FACTORY_LEFT_PATH;
+import static com.epicness.slimeland.AssetPaths.FACTORY_RIGHT_PATH;
 import static com.epicness.slimeland.AssetPaths.GREEN_ORNAMENT_PATH;
 import static com.epicness.slimeland.AssetPaths.ORANGE_ORNAMENT_PATH;
 import static com.epicness.slimeland.AssetPaths.PURPLE_ORNAMENT_PATH;
 import static com.epicness.slimeland.AssetPaths.RED_ORNAMENT_PATH;
 import static com.epicness.slimeland.AssetPaths.SLIME_LEFT_HALF_PATH;
 import static com.epicness.slimeland.AssetPaths.SLIME_RIGHT_HALF_PATH;
+import static com.epicness.slimeland.AssetPaths.TOWER_LEFT_PATH;
+import static com.epicness.slimeland.AssetPaths.TOWER_RIGHT_PATH;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -18,6 +22,7 @@ public class GameAssets extends Assets {
 
     // Sprites
     private Sprite leftSlime, rightSlime, cloud, cloudShadow;
+    private Sprite factoryLeft, factoryRight, towerLeft, towerRight;
     private Sprite blueOrnament, greenOrnament, orangeOrnament, purpleOrnament, redOrnament;
 
     @Override
@@ -26,6 +31,12 @@ public class GameAssets extends Assets {
         assetManager.load(SLIME_RIGHT_HALF_PATH, Texture.class);
         assetManager.load(CLOUD_PATH, Texture.class);
         assetManager.load(CLOUD_SHADOW_PATH, Texture.class);
+
+        assetManager.load(FACTORY_LEFT_PATH, Texture.class);
+        assetManager.load(FACTORY_RIGHT_PATH, Texture.class);
+        assetManager.load(TOWER_LEFT_PATH, Texture.class);
+        assetManager.load(TOWER_RIGHT_PATH, Texture.class);
+
         assetManager.load(BLUE_ORNAMENT_PATH, Texture.class);
         assetManager.load(GREEN_ORNAMENT_PATH, Texture.class);
         assetManager.load(ORANGE_ORNAMENT_PATH, Texture.class);
@@ -39,6 +50,11 @@ public class GameAssets extends Assets {
         rightSlime = new Sprite(assetManager.get(SLIME_RIGHT_HALF_PATH, Texture.class));
         cloud = new Sprite(assetManager.get(CLOUD_PATH, Texture.class));
         cloudShadow = new Sprite(assetManager.get(CLOUD_SHADOW_PATH, Texture.class));
+
+        factoryLeft = new Sprite(assetManager.get(FACTORY_LEFT_PATH, Texture.class));
+        factoryRight = new Sprite(assetManager.get(FACTORY_RIGHT_PATH, Texture.class));
+        towerLeft = new Sprite(assetManager.get(TOWER_LEFT_PATH, Texture.class));
+        towerRight = new Sprite(assetManager.get(TOWER_RIGHT_PATH, Texture.class));
 
         blueOrnament = new Sprite(assetManager.get(BLUE_ORNAMENT_PATH, Texture.class));
         greenOrnament = new Sprite(assetManager.get(BLUE_ORNAMENT_PATH, Texture.class));
@@ -62,6 +78,22 @@ public class GameAssets extends Assets {
 
     public Sprite getCloudShadow() {
         return cloudShadow;
+    }
+
+    public Sprite getFactoryLeft() {
+        return factoryLeft;
+    }
+
+    public Sprite getFactoryRight() {
+        return factoryRight;
+    }
+
+    public Sprite getTowerLeft() {
+        return towerLeft;
+    }
+
+    public Sprite getTowerRight() {
+        return towerRight;
     }
 
     public Sprite getBlueOrnament() {
